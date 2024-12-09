2404e42cb3c9096c8d5e119ef50f0b31f3552325
Anton Prince Oommen <antonprince95@gmail.com>
2024-12-10 01:50:30 +0530

    final final fnal

--- frontend/src/Packet.jsx
+++ frontend/src/Packet.jsx
@@ -78,135 +78,84 @@ const Packet = () => {
         }
     };
 
-  //   async function sendFeedToServer(video) {
-  //     const canvas = document.createElement('canvas');
-  //     const context = canvas.getContext('2d');
+    async function sendFeedToServer(video) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      let feedwebsocket, objectwebsocket;
+      const reconnectInterval = 500; // 500 ms
   
-  //     let feedwebsocket, objectwebsocket;
-  //     const reconnectInterval = 500; // 500 ms
-  
-  //     function connectFeedWebSocket() {
-  //         feedwebsocket = new WebSocket(wss://${host}/ws/camera_feed_expiry);
+function connectFeedWebSocket() {
+          feedwebsocket = new WebSocket(`wss://backend.angeloantu.online/ws/camera_feed_fruit`);
           
-  //         feedwebsocket.onopen = () => {
-  //             console.log('Feed WebSocket connected');
-  //             setInterval(() => {
-  //                 if (video.readyState === 4 && video.videoWidth && video.videoHeight) {
-  //                     canvas.width = video.videoWidth;
-  //                     canvas.height = video.videoHeight;
-  //                     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-  //                     const imageData = canvas.toDataURL('image/jpeg'); // Capture frame as JPEG
+          feedwebsocket.onopen = () => {
+              console.log('Feed WebSocket connected');
+              setInterval(() => {
+                  if (video.readyState === 4 && video.videoWidth && video.videoHeight) {
+                      canvas.width = video.videoWidth;
+                      canvas.height = video.videoHeight;
+                      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+                      const imageData = canvas.toDataURL('image/jpeg'); // Capture frame as JPEG
   
-  //                     // Send the image data to the WebSocket server
-  //                     if (feedwebsocket.readyState === WebSocket.OPEN) {
-  //                         feedwebsocket.send(imageData);
-  //                     }
-  //                 }
-  //             }, 150); // Capture and send at a delay
-  //         };
+                      // Send the image data to the WebSocket server
+                      if (feedwebsocket.readyState === WebSocket.OPEN) {
+                          feedwebsocket.send(imageData);
+                      }
+                  }
+              }, 500); // Capture and send at a delay
+          };
   
-  //         feedwebsocket.onerror = (error) => {
-  //             console.error('Feed WebSocket error:', error);
-  //         };
+          feedwebsocket.onerror = (error) => {
+              console.error('Feed WebSocket error:', error);
+          };
   
-  //         feedwebsocket.onclose = () => {
-  //             console.log('Feed WebSocket connection closed. Reconnecting...');
-  //             setTimeout(connectFeedWebSocket, reconnectInterval);
-  //         };
+          feedwebsocket.onclose = () => {
+              console.log('Feed WebSocket connection closed. Reconnecting...');
+              setTimeout(connectFeedWebSocket, reconnectInterval);
+          };
   
-  //         feedwebsocket.onmessage = (event) => {
-  //             // Set the source of the img element to the received image
-  //             const imageFeed = document.getElementById('cameraFeed');
-  //             imageFeed.src = event.data; // Set the source to the received image
-  //         };
-  //     }
+          feedwebsocket.onmessage = (event) => {
+              // Set the source of the img element to the received image
+              // const imageFeed = document.getElementById('cameraFeed');
+              const base64Image = event.data
+              receiveRef.current.src = base64Image;; // Set the source to the received image
+          };
+      }
   
-  //     function connectObjectWebSocket() {
-  //         objectwebsocket = new WebSocket(wss://${host}/ws/packed_products_expiry);
+      function connectObjectWebSocket() {
+          objectwebsocket = new WebSocket(`wss://backend.angeloantu.online/ws/fruits`);
   
-  //         objectwebsocket.onopen = () => {
-  //             console.log("Object WebSocket connected");
-  //         };
+          objectwebsocket.onopen = () => {
+              console.log("Object WebSocket connected");
+          };
   
-  //         objectwebsocket.onerror = (error) => {
-  //             console.error("Object WebSocket error:", error);
-  //         };
+          objectwebsocket.onerror = (error) => {
+              console.error("Object WebSocket error:", error);
+          };
   
-  //         objectwebsocket.onclose = () => {
-  //             console.log("Object WebSocket connection closed. Reconnecting...");
-  //             setTimeout(connectObjectWebSocket, reconnectInterval);
-  //         };
+          objectwebsocket.onclose = () => {
+              console.log("Object WebSocket connection closed. Reconnecting...");
+              setTimeout(connectObjectWebSocket, reconnectInterval);
+          };
   
-  //         objectwebsocket.onmessage = (event) => {
-  //             const data = JSON.parse(event.data);
-  //             console.log(data['details']);
-  //             console.log("Product name:", data['product_name']);
-  //             console.log("Name detection:", data['name_detection']);
-  //             const count = data['count'];
-  //             console.log("COUNT:", count);
-  //             populateItemList(data['details']);
-  //         };
-  //     }
+         
+            objectwebsocket.onmessage = (event) => {
+            console.log("items data :",event.data);
+            try {
+              const newItem = JSON.parse(event.data)
+              setItems(newItem)
+              // console.log(items)
+            } catch (error) {
+              console.log(error)
+            }
+         
+     }
+      }
   
-  //     // Initialize both WebSocket connections
-  //     connectFeedWebSocket();
-  //     connectObjectWebSocket();
-  // }
-    const sendFeedToServer =async(video)=>{
-        let feedwebsocket = new WebSocket('wss://backend.angeloantu.online/ws/camera_feed_expiry');
-        let countwebsocket = new WebSocket('wss://backend.angeloantu.online/ws/packed_products_expiry');
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        
-        feedwebsocket.onopen = () => {
-            setInterval(() => {
-                if (video.readyState === 4 && video.videoWidth && video.videoHeight) {
-                    canvas.width = video.videoWidth;
-                    canvas.height = video.videoHeight;
-                    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-                    const imageData = canvas.toDataURL('image/jpeg'); // Capture frame as JPEG
-    
-                    // Send the image data to the WebSocket server
-                    if (feedwebsocket.readyState === WebSocket.OPEN) {
-                        feedwebsocket.send(imageData);
-                    }
-                }
-            }, 500); // Capture and send at a delay
-        };
+      // Initialize both WebSocket connections
+      connectFeedWebSocket();
+      connectObjectWebSocket();
+  }
 
-        feedwebsocket.onerror = (error) => {
-            console.error('feed WebSocket error:', error);
-        };
-        
-        feedwebsocket.onclose = () => {
-            console.log('feed WebSocket connection closed');
-        };
-
-        feedwebsocket.onmessage = (event) => {
-            // Assuming event.data is a Base64 string
-            const base64Image = event.data;
-            receiveRef.current.src = base64Image;
-        };
-    
-        countwebsocket.onerror = (error) => {
-            console.log("object socket error", error)
-        }
-    
-        countwebsocket.onclose = () => {
-            console.log("object socket closed");
-        }
-
-        countwebsocket.onmessage = (event) => {
-             console.log("items data :",event.data);
-             try {
-                const newItem = JSON.parse(event.data)
-                setItems((prev)=>[...prev, newItem])
-             } catch (error) {
-                
-             }
-            
-        }
-    }
 
   const stopCamera = () => {
     if (videoRef.current && videoRef.current.srcObject) {
@@ -316,10 +265,10 @@ const Packet = () => {
                    shadow-black p-2 h-[250px] md:h-[450px] my-auto 
                    '>
                     <h1 className='font-bold text-white text-2xl md:text-3xl text-center font-extrabold my-2'>Item List</h1>
-                    <h1 className='text-white text-center text-md md:text-2xl font-bold flex-wrap'>Count: {test.length}</h1>
+                    <h1 className='text-white text-center text-md md:text-2xl font-bold flex-wrap'>Count: {items.length}</h1>
                     {
-                        test.length > 0 ? (
-                            test.map((item, index) => (
+                        items.length > 0 ? (
+                            items.map((item, index) => (
                               <div key={index+1} className='text-white font-semibold text-xl lg:text-2xl p-1'>
                                 <h1>
                                   {index+1}.
